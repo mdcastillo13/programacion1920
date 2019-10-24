db6ef8b422367ba820510e44f48f3d6238cd17ce
mdcastillo13 <56704446+mdcastillo13@users.noreply.github.com>
2019-10-24 11:35:50 -0500

    Create Clasenumero2.java

---
+++ intorojava/src/intorojava/Clasenumero2.java
@@ -0,0 +1,37 @@
+/*
+  esta clase voy a realizar operaciones con dos numeros con operadores aritmeticos 
+
+ */ 
+package intorojava;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Clasenumero2 {
+
+    public static void main(String[] args){
+        
+        //1operaciones aritmeticas
+        int suma = 3+8;
+        int resta = 5-9;
+        double multiplicacion = 7*5;
+        int division = 8/5;
+        int residuo = 5%8;
+        
+        System.out.println("1operaciones aritmeticas ");
+        System.out.println("la suma es :"+suma);
+        System.out.println("La resta es : "+resta);
+        System.out.println("la multiplicacion es :"+ multiplicacion);
+        System.out.println("La divicion es: "+ division);
+        System.out.println("el residuo es :"+ residuo);
+        
+        
+                
+        
+                
+        
+    }
+    
+    
+}
