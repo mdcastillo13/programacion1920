4b95856ad79cff678df67a1796b64a0eeb8c4739
mdcastillo13 <56704446+mdcastillo13@users.noreply.github.com>
2019-11-15 09:49:15 -0500

    Create decirsisoniguales.java

---
+++ intorojava/src/intorojava/decirsisoniguales.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package intorojava;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class decirsisoniguales {
+     public static void main(String[] args){
+         Scanner datos=new Scanner(System.in);
+         int n1;
+         int n2;
+         System.out.println("ingresar n1");
+         n1=datos.nextInt();
+         System.out.println("ingresar n2");
+         n2=datos.nextInt();
+         if (n1==n2) {
+             System.out.println("los numeros son iguales");    
+         }else{
+             System.out.println("no son iguales");
+         }
+         
+            
+     }
+    
+}
