321265db8659e484e633736a4772b48ad4e09641
mdcastillo13 <56704446+mdcastillo13@users.noreply.github.com>
2019-11-15 19:48:09 -0500

    Create contador.java

---
+++ intorojava/src/intorojava/contador.java
@@ -0,0 +1,22 @@
+
+package intorojava;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class contador {
+    public static void main(String[] args){
+        System.out.println("trabajando con contadores");
+        //inicializar nuestro contador
+        int contador = 1 ;
+        //vamos a repetir 5 veces un proceso 
+        while(contador<=5) {
+            System.out.println("contador : "+contador);
+           contador = contador+1;//incremento de la variable contador en 1
+            
+        }
+            
+    }
+    
+}
