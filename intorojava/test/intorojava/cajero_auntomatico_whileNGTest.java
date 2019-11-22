8af4c37d905cdb224651c22cf6937a96a1b0c93f
mdcastillo13 <castillocastillomariodavid78@gmail.com>
2019-11-21 23:44:48 -0500

    Create cajero_auntomatico_whileNGTest.java

---
+++ intorojava/test/intorojava/cajero_auntomatico_whileNGTest.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package intorojava;
+
+import static org.testng.Assert.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class cajero_auntomatico_whileNGTest {
+    
+    public cajero_auntomatico_whileNGTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @BeforeMethod
+    public void setUpMethod() throws Exception {
+    }
+
+    @AfterMethod
+    public void tearDownMethod() throws Exception {
+    }
+
+    /**
+     * Test of main method, of class cajero_auntomatico_while.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        cajero_auntomatico_while.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
