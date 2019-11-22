fbe35ccfdbb05712b038a2f15b0085763af66e3b
mdcastillo13 <castillocastillomariodavid78@gmail.com>
2019-11-21 23:38:58 -0500

    Create multiplicacionnumeros.java

---
+++ intorojava/src/intorojava/multiplicacionnumeros.java
@@ -0,0 +1,33 @@
+
+
+package intorojava;
+import java.util. Scanner;
+public class multiplicacionnumeros {
+    public static void main(String[] args) {
+         Scanner datos = new Scanner(System.in);
+        System.out.println("Programa para multiplicacr n numeors");
+        int num;
+        int acum = 1;
+        boolean op = true;
+        System.out.println("ingrese un numero:");
+        num = datos.nextInt();
+        acum=acum*num;
+        while (op == true) {
+            System.out.println("Desea ingresar un nuevo numero si/no");
+            String res = datos.next();
+            res = res.toLowerCase(); // toLoweCase sirve para cambiar una respuesta de minuscula a mayuscula
+            if (res.equals("si")) {
+                System.out.println("Ingrese su nuevo numero:");
+                num = datos.nextInt();
+                acum=acum*num;
+            } else if (res.equals("no")) {
+                op = false;
+            }
+        }
+        System.out.println("la multiplicacion es:"+acum);
+    }
+}
+
+    
+    
+
