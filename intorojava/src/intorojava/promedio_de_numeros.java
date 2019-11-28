f51566c1c1bda6d4e6411f16ab60cb5adaedfe5c
mdcastillo13 <castillocastillomariodavid78@gmail.com>
2019-11-28 11:13:57 -0500

    Create promedio_de_numeros.java

---
+++ intorojava/src/intorojava/promedio_de_numeros.java
@@ -0,0 +1,31 @@
+
+package intorojava;
+import java.util.Scanner;
+
+public class promedio_de_numeros {
+    public static void main(String[] args) {
+        Scanner datos = new Scanner(System.in);
+        int num;
+        double prom = 0;
+        double suma = 0;
+        int cont =0 ;
+        boolean op = true;
+        do{
+            System.out.println("ingresar una nota si/no");
+            String res = datos.next();
+            res = res.toLowerCase(); // toLoweCase sirve para cambiar una respuesta de minuscula a mayuscula
+            if (res.equals("si")) {
+                System.out.println("Ingrese su Nota:");
+                num = datos.nextInt();
+                cont++;
+                suma=(suma+num);
+                prom = suma / cont;
+            } else if (res.equals("no")) {
+                op = false;
+            }
+        }while (op == true);
+        System.out.println("el promedio es es:"+prom);
+    }
+    
+    
+}
