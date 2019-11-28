3576abd014e6314dfa75edad86565e7437832e4d
mdcastillo13 <castillocastillomariodavid78@gmail.com>
2019-11-28 14:07:26 -0500

    Update numerosperfectos.java

--- intorojava/src/intorojava/numerosperfectos.java
+++ intorojava/src/intorojava/numerosperfectos.java
@@ -1,14 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package intorojava;
 
-/**
- *
- * @author LENOVO
- */
+
+package intorojava;
+import java.util.Scanner;
+
+
 public class numerosperfectos {
+        public static void main(String[] args) {
+
+        int i = 0;
+        int j = 0;
+        int max = 10000;
+        int acum;
+        while (i <=max){
+            i++;
+            for(i = 1; i <= max; i++ ) {
+                acum = 0;
+                for (j = 1; j< i; j++) {
+                    if (i % j == 0) {
+                        acum = acum + j;
+                     }
+                }
+                if (i == acum){
+                    System.out.println("\t"+i);
+                }
+            }
+        }
+    }
+        
+}       
+
+
     
-}
+    
+
