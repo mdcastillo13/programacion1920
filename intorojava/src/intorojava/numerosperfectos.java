8c329a4616b99ce91769615333ee12f8417740b1
mdcastillo13 <castillocastillomariodavid78@gmail.com>
2019-11-29 22:43:51 -0500

    Update numerosperfectos.java

--- intorojava/src/intorojava/numerosperfectos.java
+++ intorojava/src/intorojava/numerosperfectos.java
@@ -1,35 +1,30 @@
-
-
 package intorojava;
+
 import java.util.Scanner;
 
-
 public class numerosperfectos {
-        public static void main(String[] args) {
 
-        int i = 0;
-        int j = 0;
-        int max = 10000;
+    public static void main(String[] args) {
+
+        int num = 0;
+        int div = 0;
         int acum;
-        while (i <=max){
-            i++;
-            for(i = 1; i <= max; i++ ) {
-                acum = 0;
-                for (j = 1; j< i; j++) {
-                    if (i % j == 0) {
-                        acum = acum + j;
-                     }
+            while (num<= 4) {
+                num++;
+                for (num = 1; num <= 10000; num++) {
+                    acum = 0;
+                    for(div = 1; div < num; div++) {
+                        if (num % div==0) {
+                            acum = acum + div;
+                        }
+                    }
+                    if (num == acum) {
+                        System.out.println("\t" + num);
+                    }
                 }
-                if (i == acum){
-                    System.out.println("\t"+i);
-                }
-            }
+            
         }
+
     }
-        
-}       
 
-
-    
-    
-
+}
