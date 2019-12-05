5c07016328d6ea6ab727e068ce4f789d843adb76
mdcastillo13 <castillocastillomariodavid78@gmail.com>
2019-12-05 12:03:58 -0500

    Create numerodefilas.java

---
+++ intorojava/src/intorojava/numerodefilas.java
@@ -0,0 +1,22 @@
+
+package intorojava;
+
+import java.util.Scanner;
+
+
+public class numerodefilas {
+    public static void main(String[] args){
+         Scanner datos=new Scanner(System.in);
+         int c = 1;
+         String cadena = "";
+         
+         while(c<=4){
+            
+             cadena += c;
+             System.out.println(cadena);
+             c = c + 1;
+         }
+         
+    }
+  }  
+
