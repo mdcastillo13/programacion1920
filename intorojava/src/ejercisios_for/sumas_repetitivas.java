e86f937326a4f5bb32b2d93cad5c8d42b68eeb0b
mdcastillo13 <castillocastillomariodavid78@gmail.com>
2019-12-13 21:41:04 -0500

    Create sumas_repetitivas.java

---
+++ intorojava/src/ejercisios_for/sumas_repetitivas.java
@@ -0,0 +1,20 @@
+
+package ejercisios_for;
+
+import java.util.Scanner;
+
+
+public class sumas_repetitivas {
+    public static void main(String[] args){
+         Scanner datos=new Scanner(System.in);
+         int num=4;
+         int num2=3;
+         int acumulador=0;
+         for (int cnt = 1; cnt<=num;cnt++){
+             acumulador = acumulador + num2;
+         }
+         System.out.println("presentar(acumulador"+ acumulador );
+
+      }           
+    
+}
