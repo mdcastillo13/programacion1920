58ca20d6ffb8629b39a6aafcb066ec503370cb92
mdcastillo13 <castillocastillomariodavid78@gmail.com>
2019-12-13 21:42:57 -0500

    Create divicion_for.java

---
+++ intorojava/src/ejercisios_for/divicion_for.java
@@ -0,0 +1,19 @@
+
+package ejercisios_for;
+
+
+public class divicion_for{
+      public static void main(String[] args){
+      int div=7;
+      int divisor=-2;
+      int cont;
+      for(cont = 1; div >=divisor; cont++){
+          div= div- divisor;
+      }
+          System.out.println("cont");
+     
+
+        
+    
+  }  
+}
