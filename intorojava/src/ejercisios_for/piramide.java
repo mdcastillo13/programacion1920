fd4ab584f5adeedcec1c727e264f0f80735e0355
mdcastillo13 <castillocastillomariodavid78@gmail.com>
2019-12-13 21:42:45 -0500

    Create piramide.java

---
+++ intorojava/src/ejercisios_for/piramide.java
@@ -0,0 +1,25 @@
+
+package ejercisios_for;
+
+import java.util.Scanner;
+
+
+public class piramide {
+    public static void main(String[] args) {
+        Scanner datos = new Scanner(System.in);
+          int c = 1;
+         String cadena = "";
+         
+         for(;c<=5; c = c + 1){
+            
+             cadena += c;
+             System.out.println(cadena);
+             
+         }
+         
+    }
+  }  
+
+    
+    
+
